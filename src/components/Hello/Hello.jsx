e167c0332edec53e6fa643f299bca1e7d8941cfa
Vitaly Rtishchev <rtivital@gmail.com>
2016-11-13 20:53:47 +0300

    remove old Hello component

--- src/components/Hello/Hello.jsx
+++ src/components/Hello/Hello.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import Card from './Card';
 import './hello.scss';
-import content from './content';
 
-const Hello = () => {
-  const data = content.map(
-    (part, index) => <Card {...part} key={`${index}-${part.image}`} />
-  );
+const GH_LINK = 'https://github.com/rtivital/react-challenge-sort-and-search';
 
-  return <div className="entry">{data}</div>;
-};
+const Hello = () => (
+  <div className="hello">
+    <h1 className="hello__title">Добро пожаловать в мир React</h1>
+    <p>
+      Ты запустил стартовый шаблон, а значит, половина дела уже сделана!
+      Не забудь поставить проекту звезду на <a href={GH_LINK}>Github</a>.
+      <strike> Ура, вот и вторая половина сделана, ты молодец!).</strike>
+    </p>
+  </div>
+);
 
 export default Hello;
